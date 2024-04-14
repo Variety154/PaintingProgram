7ed08df8089523ec580ddfd8ec043040ac5b00fe
Variety154 <varvarakit15042103@gmail.com>
2024-04-14 17:19:12 +0300

    9 спринт

---
+++ PaintingProgram/Profile/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  PaintingProgram
+//
+//  Created by Varvara Kiseleva on 02.04.2024.
+//
+
+import Foundation
---
+++ PaintingProgram/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  PaintingProgram
+//
+//  Created by Varvara Kiseleva on 03.04.2024.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
