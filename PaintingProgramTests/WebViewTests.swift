40a497e23e0b53701dd950f47604687bdd0c03e6
Variety154 <varvarakit15042103@gmail.com>
2024-09-25 22:43:39 +0300

    13 спринт

--- PaintingProgramTests/WebViewTests.swift
+++ PaintingProgramTests/WebViewTests.swift
@@ -1,8 +1,110 @@
 //
-//  WebViewTests.swift
-//  PaintingProgram
+//  PaintingProgramTests.swift
+//  PaintingProgramTests
 //
-//  Created by Varvara Kiseleva on 13.08.2024.
+//  Created by Varvara Kiseleva on 23.09.2024.
 //
 
+@testable import PaintingProgram
+import XCTest
+import PaintingProgram
 import Foundation
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var view: WebViewViewControllerProtocol?
+    var viewDidLoadCalled: Bool = false
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    func didUpdateProgressValue(_ newValue: Double) { }
+    func code(from url: URL) -> String? { return nil }
+}
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: PaintingProgram.WebViewPresenterProtocol?
+    var loadRequestCalled: Bool = false
+    
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+    func setProgressValue(_ newValue: Float) { }
+    func setProgressHidden(_ isHidden: Bool) { }
+}
+
+
+final class WebViewTests: XCTestCase {
+    func testViewControllerCallsViewDidLoad() {
+        
+        let viewController = WebViewViewController()
+        let presenter = WebViewPresenterSpy()
+        
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        _ = viewController.view
+     
+        XCTAssertTrue(presenter.viewDidLoadCalled)
+    }
+    
+    func testPresenterCallsLoadRequest() {
+        
+        let viewController = WebViewViewControllerSpy()
+        let presenter = WebViewPresenter(authHelper: AuthHelper())
+        
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        presenter.viewDidLoad()
+        
+        XCTAssertTrue(viewController.loadRequestCalled)
+    }
+    
+    func testProgressVisibleWhenLessThenOne() {
+        
+        let presenter = WebViewPresenter(authHelper: AuthHelper())
+        let progress: Float = 0.6
+        
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        XCTAssertFalse(shouldHideProgress)
+    }
+    
+    func testProgressVisibleWhenOne() {
+        
+        let presenter = WebViewPresenter(authHelper: AuthHelper())
+        let progress: Float = 1
+        
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        XCTAssertTrue(shouldHideProgress)
+    }
+    
+    func testAuthHelperAuthURL() {
+        
+        let configuration = AuthConfiguration.standard
+        let authHelper = AuthHelper(configuration: configuration)
+        
+        let url = authHelper.authURL()
+        let urlString = url!.absoluteString
+        
+        XCTAssertTrue(urlString.contains(configuration.accessKey))
+        XCTAssertTrue(urlString.contains(configuration.accessScope))
+        XCTAssertTrue(urlString.contains(configuration.authURLString))
+        XCTAssertTrue(urlString.contains(configuration.redirectURI))
+        XCTAssertTrue(urlString.contains("code"))
+    }
+    
+    func testCodeFromURL() {
+        
+        let authHelper = AuthHelper()
+        var urlComponents = URLComponents(string: "https://unsplash.com/oauth/authorize/native")!
+        urlComponents.queryItems = [URLQueryItem(name: "code", value: "test code")]
+        let url = urlComponents.url!
+        
+        let code = authHelper.code(from: url)
+        
+        XCTAssertEqual(code, "test code")
+    }
+}
+
--- PaintingProgramUITests/PaintingProgramUITests.swift
+++ PaintingProgramUITests/PaintingProgramUITests.swift
@@ -1,13 +1,13 @@
 //
-//  Painting_ProgramUITests.swift
-//  Painting ProgramUITests
+//  PaintingProgramUITests.swift
+//  PaintingProgramUITests
 //
 //  Created by Varvara Kiseleva on 23.09.2024.
 //
 
 import XCTest
 
-final class Painting_ProgramUITests: XCTestCase {
+final class PaintingProgramUITests: XCTestCase {
     private let app = XCUIApplication()
     
     override func setUpWithError() throws {
@@ -18,71 +18,72 @@ final class Painting_ProgramUITests: XCTestCase {
     
     func testAuth() throws {
         app.buttons["Authenticate"].tap()
-        
         let webView = app.webViews["UnsplashWebView"]
-        
         XCTAssertTrue(webView.waitForExistence(timeout: 5))
         
         let loginTextField = webView.descendants(matching: .textField).element
         XCTAssertTrue(loginTextField.waitForExistence(timeout: 5))
-        
         loginTextField.tap()
-        loginTextField.typeText("<Ваш e-mail>")
+        loginTextField.typeText("")
+        app.toolbars["Toolbar"].buttons["Done"].tap()
         webView.swipeUp()
         
         let passwordTextField = webView.descendants(matching: .secureTextField).element
         XCTAssertTrue(passwordTextField.waitForExistence(timeout: 5))
         
         passwordTextField.tap()
-        passwordTextField.typeText("<Ваш пароль>")
-        webView.swipeUp()
+        passwordTextField.tap()
+        passwordTextField.typeText("")
         
+        app.toolbars["Toolbar"].buttons["Done"].tap()
+        
+        sleep(1)
         webView.buttons["Login"].tap()
+        sleep(5)
         
+        let tablesQuery = app.tables
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        XCTAssertTrue(cell.waitForExistence(timeout: 5))
+        
+            print(app.debugDescription)
+    }
+
+    
+    func testFeed() throws {
         let tablesQuery = app.tables
         let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
         
         XCTAssertTrue(cell.waitForExistence(timeout: 5))
-    }
-    
-    func testFeed() throws {
-        let tablesQuery = app.tables
-        
-        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
         cell.swipeUp()
         
+        let cellTarget = tablesQuery.children(matching: .cell).element(boundBy: 1)
+        
+        cellTarget.buttons["likeButton"].tap()
+        cellTarget.buttons["likeButton"].tap()
         sleep(2)
         
-        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
-        
-        cellToLike.buttons["like button off"].tap()
-        cellToLike.buttons["like button on"].tap()
-        
-        sleep(2)
-        
-        cellToLike.tap()
-        
-        sleep(2)
+        cellTarget.tap()
+        sleep(3)
         
         let image = app.scrollViews.images.element(boundBy: 0)
-        // Zoom in
-        image.pinch(withScale: 3, velocity: 1) // zoom in
-        // Zoom out
+        image.pinch(withScale: 3, velocity: 1)
         image.pinch(withScale: 0.5, velocity: -1)
+        sleep(2)
         
-        let navBackButtonWhiteButton = app.buttons["nav back button white"]
-        navBackButtonWhiteButton.tap()
+        app.buttons["SingleBackButton"].tap()
     }
     
     func testProfile() throws {
-        sleep(3)
+        let tablesQuery = app.tables
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        XCTAssertTrue(cell.waitForExistence(timeout: 5))
+        
         app.tabBars.buttons.element(boundBy: 1).tap()
+        XCTAssertTrue(app.staticTexts["Sergey Baskakov"].exists)
+        XCTAssertTrue(app.staticTexts["@s1zzen"].exists)
         
-        XCTAssertTrue(app.staticTexts["Name Lastname"].exists)
-        XCTAssertTrue(app.staticTexts["@username"].exists)
+        app.buttons["ProfileLogoutButton"].tap()
         
-        app.buttons["logout button"].tap()
-        
-        app.alerts["Bye bye!"].scrollViews.otherElements.buttons["Yes"].tap()
+        app.alerts["Пока, пока!"].scrollViews.otherElements.buttons["Да"].tap()
     }
 }
